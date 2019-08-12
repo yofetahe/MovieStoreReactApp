20c3fc523865de496dd62a6471bbb6eaa0297e13
Yofatehe <42419407+yofetahe@users.noreply.github.com>
2019-08-12 09:26:13 -0700

    Updates

---
+++ precache-manifest.c5a6f355f69ed8c10b65f3c5a5ec1eda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17ee9df55addd94bdc127efee84a59b",
+    "url": "/MovieStoreReactApp/index.html"
+  },
+  {
+    "revision": "d2453ce41d95bc499fef",
+    "url": "/MovieStoreReactApp/static/css/main.8283d259.chunk.css"
+  },
+  {
+    "revision": "01b71919244d8ae7a0cc",
+    "url": "/MovieStoreReactApp/static/js/2.b1d25567.chunk.js"
+  },
+  {
+    "revision": "d2453ce41d95bc499fef",
+    "url": "/MovieStoreReactApp/static/js/main.fda21d95.chunk.js"
+  },
+  {
+    "revision": "054b94d22dbd9765c662",
+    "url": "/MovieStoreReactApp/static/js/runtime~main.78259a53.js"
+  }
+]);
